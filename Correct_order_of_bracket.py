eabd5dbf59dd919ff04f6994af024ff5ad1d3b46
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-12-04 20:07:54 +0530

    Addedd2

--- Correct_order_of_bracket.py
+++ Correct_order_of_bracket.py
@@ -3,19 +3,15 @@ def is_valid(s):
     pairs = {')': '(', '}': '{', ']': '['}
 
     for ch in s:
-        # If opening bracket → push to stack
         if ch in "({[":
             stack.append(ch)
         else:
-            # Closing bracket but stack empty
             if not stack:
                 return False
-            
-            # If top of stack does not match
+
             if stack[-1] != pairs[ch]:
                 return False
             
-            # Pop matched bracket
             stack.pop()
 
     return len(stack) == 0
